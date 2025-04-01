7f705408389d7070d82de73c6c7d33cbabb85bc0
HelloWorld7894 <danpojhan@protonmail.com>
2025-04-01 22:46:06 +0200

    Reworked this into tabs because sphinx-design was not getting very well with the tabs

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -17,8 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx_tabs.tabs',
-    'sphinx_design'
+    'sphinx_tabs.tabs'
 ]
 
 intersphinx_mapping = {
