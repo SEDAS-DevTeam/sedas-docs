f8fe71e608570c7dc28ff73e8c11fe2968677c33
HelloWorld7894 <danpojhan@protonmail.com>
2025-04-19 15:23:35 +0200

    experimenting with automatic translations

---
+++ inference.py
@@ -0,0 +1,124 @@
+from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
+import os
+
+
+class Translator:
+    # currently using M2M100_418M model from Meta AI https://huggingface.co/facebook/m2m100_418M
+    def __init__(self, lang):
+        self.checkpoint = "facebook/m2m100_418M"
+        self.model = M2M100ForConditionalGeneration.from_pretrained(self.checkpoint)
+        self.tokenizer = M2M100Tokenizer.from_pretrained(self.checkpoint)
+        self.tokenizer.src_lang = "en"
+        self.out_lang = lang
+
+    def run(self, text):
+        encoded = self.tokenizer(text, return_tensors="pt")
+        generated_tokens = self.model.generate(**encoded, forced_bos_token_id=self.tokenizer.get_lang_id(self.out_lang))
+
+        out = self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+        return out[0]
+
+
+class Rules:
+    @staticmethod
+    def is_header(line): #
+        if "===" in line or "---" in line or "~~~" in line or "^^^" in line: return True
+
+    @staticmethod
+    def is_link_def(line):
+        if "#." in line and ":ref:" in line: return True
+
+    @staticmethod
+    def is_link(line):
+        if ".. _" in line and line[-1] == ":": return True
+
+    @staticmethod
+    def is_directive(line): #
+        if ".." in line and "::" in line: return True
+
+    @staticmethod
+    def is_parameter(line): #
+        if line[0] == ":" and line[-1] == ":": return True
+
+    @staticmethod
+    def is_code_block(line): #
+        if ".. code-block::" in line: return True
+
+    @staticmethod
+    def is_table_block(line):
+        if ".. list-table::" in line: return True
+
+    @staticmethod
+    def is_blank(line): #
+        if len(line) == 0: return True
+
+
+class RstIterator:
+    def __init__(self, path):
+        self.path = path
+
+    def iterate(self, model: Translator):
+        for file in os.listdir(self.path):
+            if ".rst" not in file:
+                continue
+
+            in_code_block = False
+            in_table_block = False
+            is_space = False
+            temp_iter = 0
+            inp_lines = []
+            translated_lines = []
+
+            print(f"Translating {file}")
+
+            file_path = os.path.join(self.path, file)
+
+            # Read file contents
+            with open(file_path, "r", encoding="utf-8") as rst_file:
+                for line in rst_file:
+                    inp_lines.append(line.strip())
+
+            # Overwrite
+            with open(file_path, "w", encoding="utf-8") as rst_file:
+                for line_input in inp_lines:
+                    if Rules.is_code_block(line_input):
+                        in_code_block = True
+                        temp_iter = 0
+                    if Rules.is_table_block(line_input):
+                        in_table_block = True
+                        temp_iter = 0
+                    if Rules.is_blank(line_input):
+                        # Checking if code block exit/table block exit or just a newline before code
+
+                        if temp_iter != 1 and in_code_block:
+                            in_code_block = False
+                        elif temp_iter != 1 and in_table_block:
+                            in_table_block = False
+                        else:
+                            # still behaves like a space
+                            is_space = True
+
+                    # first checking blank spaces and code blocks, then directives
+                    if in_code_block or in_table_block or is_space:
+                        translated_lines.append(line_input + "\n")
+                        is_space = False
+                    elif Rules.is_directive(line_input) or \
+                        Rules.is_parameter(line_input) or \
+                        Rules.is_header(line_input) or \
+                        Rules.is_link(line_input) or \
+                            Rules.is_link_def(line_input):
+                        translated_lines.append(line_input + "\n")
+                    else:
+                        # line is passing rules
+                        model_out = model.run(line_input)
+                        translated_lines.append(model_out + "\n")
+
+                    temp_iter += 1
+
+                rst_file.writelines(translated_lines)
+
+
+if __name__ == "__main__":
+    model = Translator()
+    output = model.run("This is a SEDAS-manager test")
+    print(output)
--- tasks.py
+++ tasks.py
@@ -2,6 +2,11 @@ from invoke import task
 from pathlib import Path
 from os import path, chdir
 
+import shutil
+import yaml
+
+from inference import Translator, RstIterator
+
 abs_path = str(Path(__file__).parent)
 
 PURPLE = '\033[0;35m'
@@ -12,9 +17,25 @@ def print_color(color, text):
     print(color + text + NC)
 
 
+def input_color(color, text):
+    return input(color + text + NC)
+
+
+def read_yaml_config(path):
+    with open(path) as file:
+        try:
+            return yaml.safe_load(file)
+        except yaml.YAMLError as e:
+            print(e)
+
+
+def write_yaml_config(path, content):
+    with open(path, "w") as file:
+        yaml.dump(content, file, default_flow_style=False)
+
+
 @task
 def build(ctx, type):
-    print(type)
     if type != "en" and type != "cz":
         print_color(PURPLE, "No documentation build was selected! (pick: en, cz)")
         return
@@ -27,3 +48,40 @@ def build(ctx, type):
     print_color(PURPLE, f"Building {type} documentation...")
     ctx.run("make html", pty=True)
     print_color(PURPLE, f"Done, link here: {doclink}")
+
+
+@task
+def create_translation(ctx, type):
+    en_doc_path = path.join(abs_path, "docs/en")
+    sel_doc_path = path.join(abs_path, f"docs/{type}")
+
+    print_color(PURPLE, "Copying resource files...")
+    shutil.copytree(en_doc_path, sel_doc_path)
+
+    print_color(PURPLE, "Changing configurations...")
+    config_path = path.join(sel_doc_path, ".readthedocs.yaml")
+    trans_config = read_yaml_config(config_path)
+
+    # change some keys in translation config
+    trans_config["python"]["install"][0]["requirements"] = f"docs/{type}/requirements.txt"
+    trans_config["sphinx"]["configuration"] = f"docs/{type}/source/conf.py"
+
+    write_yaml_config(config_path, trans_config)
+
+    print_color(PURPLE, "Running model inference for automatic translation...")
+
+    rst_sources_path = path.join(sel_doc_path, "source")
+    model = Translator(type)
+    iterator = RstIterator(rst_sources_path)
+    iterator.iterate(model)
+
+    print_color(PURPLE, "Done!")
+
+
+@task
+def delete_translation(ctx, type):
+    print_color(PURPLE, f"Deleting translation: {type}")
+
+    sel_doc_path = path.join(abs_path, f"docs/{type}")
+    if input_color(PURPLE, f"Are you sure you want to remove {type}? (y/n): ") == "y": shutil.rmtree(sel_doc_path)
+    else: print_color(PURPLE, "Operation cancelled by user")
